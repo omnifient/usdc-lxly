7e536246e1a0c70a81cac351a00b4209edf30506
omnifient <90161786+omnifient@users.noreply.github.com>
2023-07-19 19:18:10 +0100

    implement the NativeConverter contract (wip)

--- src/NativeConverter.sol
+++ src/NativeConverter.sol
@@ -2,21 +2,27 @@
 pragma solidity ^0.8.17;
 
 import "@zkevm/interfaces/IPolygonZkEVMBridge.sol";
+import {Ownable} from "@oz/access/Ownable.sol";
+import {Pausable} from "@oz/security/Pausable.sol";
+import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";
+
+import {IUSDC} from "./interfaces/IUSDC.sol";
 
 // This contract will receive BridgeWrappedUSDC on zkEVM and issue USDC.e on zkEVM.
 // This contract will hold the minter role giving it the ability to mint USDC.e based on inflows of BridgeWrappedUSDC.
 // This contract will also have a permissionless publicly callable function called “migrate” which when called will
 // withdraw all BridgedWrappedUSDC to L1 via the LXLY bridge. The beneficiary address will be the L1Escrow,
 // thus migrating the supply and settling the balance.
-contract NativeConverter {
+contract NativeConverter is Ownable, Pausable {
     // TODO: upgradeable
-    // TODO: pausable
+
+    using SafeERC20 for IUSDC;
 
     IPolygonZkEVMBridge public immutable bridge;
     uint32 public immutable l1ChainId;
     address public immutable l1Escrow;
-    address public immutable zkUSDCe; // TODO: replace with IUSDC
-    address public immutable zkBWUSDC; // TODO: ERC20
+    IUSDC public immutable zkUSDCe;
+    IUSDC public immutable zkBWUSDC;
 
     constructor(
         IPolygonZkEVMBridge bridge_,
@@ -25,40 +31,49 @@ contract NativeConverter {
         address zkUSDCe_,
         address zkBWUSDC_
     ) {
-        // TODO: TBI
         bridge = bridge_;
         l1ChainId = l1ChainId_;
         l1Escrow = l1Escrow_;
-        zkUSDCe = zkUSDCe_;
-        zkBWUSDC = zkBWUSDC_;
+        zkUSDCe = IUSDC(zkUSDCe_);
+        zkBWUSDC = IUSDC(zkBWUSDC_);
     }
 
-    function convert(uint256 amount, address receiver) external {
+    function convert(uint256 amount, address receiver) external whenNotPaused {
         // User calls convert() on NativeConverter,
         // BridgeWrappedUSDC is transferred to NativeConverter
         // NativeConverter calls mint() on NativeUSDC which mints
         // new supply to the correct address.
 
-        // TODO: TBI
-
         require(receiver != address(0), "INVALID_RECEIVER");
         require(amount > 0, "INVALID_AMOUNT");
 
-        // TODO: BridgeWrappedUSDC is transferred to NativeConverter
-        // zkBWUSDC.safeTransferFrom(msg.sender, address(this), amount);
-
-        // TODO: calls mint() on NativeUSDC which mints new supply to the correct address.
-        // zkUSDCe.mint(receiver, amount);
+        // transfer the wrapped usdc to the converter, and mint back native usdc
+        zkBWUSDC.safeTransferFrom(msg.sender, address(this), amount);
+        zkUSDCe.mint(receiver, amount);
     }
 
-    function migrate() external {
+    function migrate() external whenNotPaused {
         // Anyone can call migrate() on NativeConverter to
         // have all BridgeWrappedUSDC withdrawn via the zkEVMBridge
         // moving the L1_USDC held in the zkEVMBridge to L1Escrow
 
         // TODO: TBD and TBI
-
-        bytes memory data = abi.encode("TBD");
+        uint256 amount = zkBWUSDC.balanceOf(address(this));
+        // bytes memory data = abi.encode(l1Escrow, amount);
         // bridge.bridgeMessage(l1ChainId, l1Escrow, true, data); // TODO: forceUpdateGlobalExitRoot TBD
+    }
+
+    /**
+     * @dev called by the owner to pause, triggers stopped state
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev called by the owner to unpause, returns to normal state
+     */
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
